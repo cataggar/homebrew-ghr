7ae9126d73594ff187fb75527c4a42ebd5f7f7b0
Cameron Taggart <cataggar@outlook.com>
2026-06-03 05:59:39 +0000

    Update ghr formula to v0.4.0

--- Formula/ghr.rb
+++ Formula/ghr.rb
@@ -1,28 +1,28 @@
 class Ghr < Formula
   desc "Installer for GitHub releases"
   homepage "https://github.com/cataggar/ghr"
-  version "0.3.2"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/cataggar/ghr/releases/download/v#{version}/ghr-#{version}-macos-arm64.tar.gz"
-      sha256 "148df9aee06978918da79c6d609af9a8ebe7045ec619f857b7d998ba9c7727b2"
+      sha256 "eb826495329ec8ac87f02006844c7b09cb48290aa341e5ad038f884c87829134"
     end
     on_intel do
       url "https://github.com/cataggar/ghr/releases/download/v#{version}/ghr-#{version}-macos-x64.tar.gz"
-      sha256 "a54fa19e2e0c2a57aff021ee089ba746493e07cbd17530e8b5018f5890b4b565"
+      sha256 "c6d78812fcaefd45b47d70e2e7181ae060a97b45d06b9d0e29e6380664331367"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/cataggar/ghr/releases/download/v#{version}/ghr-#{version}-linux-arm64.tar.gz"
-      sha256 "3218d35b5deef3b8fcba0115da812853a6bf7b3222681c3a00a6991dba9e736b"
+      sha256 "4d6bbf26f6926dc1d32b8bc2cdcd6c9407f7ea367a3b8610ec58b1b89c6e073a"
     end
     on_intel do
       url "https://github.com/cataggar/ghr/releases/download/v#{version}/ghr-#{version}-linux-x64.tar.gz"
-      sha256 "eb22090233fa4d2eaa98931ecc3c089c6340595529f7b5016d30818a11d3686e"
+      sha256 "0041f94f96b5114178903ed5c3f7f7cdb874855dae578830ab5e1389ec813910"
     end
   end
 
